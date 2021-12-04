98bb893cf76e95c8f9fa006e24ce7eba8cbcc9f9
Bantish Ramburn <78910746+bantishramburn@users.noreply.github.com>
2021-12-04 19:23:10 +0400

    expenses, added fonts

--- js/main.js
+++ js/main.js
@@ -10,15 +10,30 @@ let incomes=[/*{
 }*/
 ];
 //array of incomes as we can have multiple income sources
-let expenses=[/*{
-    description:'Eat dholl puri',
-    amount:25,
-    date: 2021-11-27,
-    category:'Eating out'
-}*/];
-//array of expenses
+let expenses=[
+    {
+        description:'Eat dholl puri',
+        amount:25,
+        date: 2021-11-27,
+        category:'Eating out',
+     
+    },
+    {
+        description:'Car insurance',
+        amount:10000,
+        date: 2021-11-30,
+        category:'Car',
+      
+    },
+    {
+        description:'Paid home loan',
+        amount:15000.50,
+        date: 2021-11-30,
+        category:'Loan',
+     
 
-//default categories for expenses
+    }
+]
 let categories=[
     {
         name:'Car',
@@ -53,20 +68,63 @@ let categories=[
 
 function render(){ // this function is called after each action performed ex: after adding an expense or after adding an income or any other action that requires the data on screen to be updated.
     
+    document.querySelector('#tab-categories .content').innerHTML="";
+    //rendering categories
     for(let i in categories){
-    const newDiv = document.createElement("div");
-    newDiv.classList.add(`cat_${i}`);
-    newDiv.classList.add(`category`);
-    if(categories[i].default==true)
-    newDiv.classList.add(`default`);
-  
-    // and give it some content
-    const newContent = document.createTextNode(categories[i].name);
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-    document.querySelector('#tab-categories .content').append(newDiv);
+        const newDiv = document.createElement("div");
+        newDiv.classList.add(`cat_${i}`);
+        newDiv.classList.add(`category`);
+        if(categories[i].default==true){
+            newDiv.classList.add(`default`);
+        }
+
+        // and give it some content
+        const newContent = document.createTextNode(categories[i].name);
+    
+        // add the text node to the newly created div
+        newDiv.appendChild(newContent);
+        document.querySelector('#tab-categories .content').append(newDiv);
     }
+
+    //rendering expenses
+    for(let i in expenses){
+        const newDiv = document.createElement("div");
+        newDiv.classList.add(`exp_${i}`);
+        newDiv.classList.add(`expense`);
+        //element to for icon of category
+        const iconDiv= document.createElement("div");
+        iconDiv.classList.add('column-1');
+
+        //element for category title and expense description 
+        const expenseDiv= document.createElement("div");
+        expenseDiv.classList.add('column-2');
+        //category title
+        const rowCatTitleDiv= document.createElement("div");
+        rowCatTitleDiv.classList.add('row-1');
+        rowCatTitleDiv.appendChild(document.createTextNode(expenses[i].category));
+        expenseDiv.appendChild(rowCatTitleDiv);
+        //expense desc
+        const expenseDescDiv= document.createElement("div");
+        expenseDescDiv.classList.add('row-2');
+        expenseDescDiv.appendChild(document.createTextNode(expenses[i].description));
+        expenseDiv.appendChild(expenseDescDiv);
+        
+        //element for category title and expense description 
+        const expenseAmountDiv= document.createElement("div");
+        expenseAmountDiv.classList.add('column-3');
+        const amount=document.createTextNode(`Rs ${expenses[i].amount.toFixed(2)}`);
+        expenseAmountDiv.appendChild(amount);
+        
+
+        //const newContent = document.createTextNode(expenses[i].description);
+
+        newDiv.appendChild(iconDiv);
+        newDiv.appendChild(expenseDiv);
+        newDiv.appendChild(expenseAmountDiv);
+
+        document.querySelector('#tab-expenses .content').append(newDiv);
+    }
+
 }
 window.onload=(event) => {
     render();
